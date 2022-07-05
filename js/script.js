722f2ae8ef8e4b3d4c4cf5381152aabe8cf2cbbf
chanki <bucheongosok@gmail.com>
2022-07-05 12:52:21 +0900

    카카오공유 시험

--- js/script.js
+++ js/script.js
@@ -1995,24 +1995,23 @@ function kakaoInit(){
     Kakao.Link.sendDefault({
         objectType: 'feed',
         content: {
-        title: '카카오공유하기 시 타이틀',
-        description: '카카오공유하기 시 설명',
-        imageUrl: '카카오공유하기 시 썸네일 이미지 경로',
+        title: '팀플레이어 김찬기 포트폴리오',
+        description: '2022.07.05',
+        imageUrl: 'https://img1.daumcdn.net/thumb/C428x428/?scode=mtistory2&fname=https%3A%2F%2Ftistory4.daumcdn.net%2Ftistory%2F4007905%2Fattach%2F7d95921c781e48beb4e0a1dcf829487b',
         link: {
-            mobileWebUrl: '카카오공유하기 시 클릭 후 이동 경로',
-            webUrl: '카카오공유하기 시 클릭 후 이동 경로',
+            mobileWebUrl: 'https://blackstarzck.github.io/chanki-portfolio/',
+            webUrl: 'https://blackstarzck.github.io/chanki-portfolio/',
         },
         },
         buttons: [
         {
-            title: '웹으로 보기',
+            title: '방문하기',
             link: {
-            mobileWebUrl: '카카오공유하기 시 클릭 후 이동 경로',
-            webUrl: '카카오공유하기 시 클릭 후 이동 경로',
+                mobileWebUrl: 'https://blackstarzck.github.io/chanki-portfolio/',
+                webUrl: 'https://blackstarzck.github.io/chanki-portfolio/',
             },
         },
         ],
-        // 카카오톡 미설치 시 카카오톡 설치 경로이동
         installTalk: true,
     })
 }
