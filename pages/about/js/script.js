f21aad9adc285f64a56dd24d139ff078789b1adc
chanki <bucheongosok@gmail.com>
2022-07-10 22:17:50 +0900

    수정

--- pages/about/js/script.js
+++ pages/about/js/script.js
@@ -297,7 +297,7 @@
                         console.log(cnt, total)
                         if(cnt === total) setTimeout(() => { 
                             document.body.classList.remove('before-load');
-                            setTimeout(() => { document.querySelector(".loading"), 700});
+                            setTimeout(() => { document.querySelector(".loading").remove(), 700});
                         }, 1000);
                     }
                 }
