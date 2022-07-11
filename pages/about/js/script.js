f98617c0247731c3eaae815343f3564a4391ee4c
chanki <bucheongosok@gmail.com>
2022-07-11 13:13:45 +0900

    사소한거

--- pages/about/js/script.js
+++ pages/about/js/script.js
@@ -309,7 +309,7 @@
                 bar.style.width = Math.round((cnt / total) * 100) + "%";
 
                 console.log(cnt, total)
-                if(cnt === total) setTimeout(() => { 
+                if(cnt >= total) setTimeout(() => { 
                     document.body.classList.remove('before-load');
                     handleTypeAnimation();
                     setTimeout(() => { document.querySelector(".loading").remove(), 700});
