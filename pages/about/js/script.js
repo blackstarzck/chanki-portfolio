f6ea05aff820ff08a4d95e22215c66e548cbf1f3
chanki <bucheongosok@gmail.com>
2022-07-12 18:41:49 +0900

    사솬 것

--- pages/about/js/script.js
+++ pages/about/js/script.js
@@ -309,11 +309,13 @@
                 bar.style.width = Math.round((cnt / total) * 100) + "%";
 
                 console.log(cnt, total)
-                if(cnt >= total-5 || Math.round((cnt / total) * 100) === 100 ) setTimeout(() => { 
-                    document.body.classList.remove('before-load');
-                    handleTypeAnimation();
-                    setTimeout(() => { document.querySelector(".loading").remove(), 700});
-                }, 1600);
+                if(cnt >= total-5 || Math.round((cnt / total) * 100) === 100 ){
+                    setTimeout(() => { 
+                        document.body.classList.remove('before-load');
+                        handleTypeAnimation();
+                        setTimeout(() => { document.querySelector(".loading").remove(), 700});
+                    }, 1600);
+                }
             }
         }
     }
